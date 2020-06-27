2be2d5ecc8e349663a19731a5109403836848f40
Greg Low <greg@sqldownunder.com>
2020-06-27 10:44:34 +0000

    Added Sequence_DataModel_CustomerOrderLineKey.sql

---
+++ Sequence_DataModel_CustomerOrderLineKey.sql
@@ -0,0 +1,11 @@
+IF NOT EXISTS (SELECT 1 FROM sys.sequences AS seq
+                        INNER JOIN sys.schemas AS sch
+                        ON sch.schema_id = seq.schema_id 
+                        WHERE sch.[name] = N'DataModel'
+                        AND seq.[name] = N'CustomerOrderLineKey')
+BEGIN
+    DECLARE @SQL nvarchar(max) = N'CREATE SEQUENCE DataModel.CustomerOrderLineKey AS int START WITH 1;';
+    EXEC (@SQL);
+END;
+GO
+
