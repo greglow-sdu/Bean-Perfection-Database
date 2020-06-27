4d13f956e6c91f89f2e831334047018f2633fdfc
Greg Low <greg@sqldownunder.com>
2020-06-27 10:50:36 +0000

    Added Table_DataModel_CustomerOrders.sql

---
+++ Table_DataModel_CustomerOrders.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS DataModel.CustomerOrders;
+GO
+
+CREATE TABLE DataModel.CustomerOrders
+(
+    CustomerOrderKey int NOT NULL
+        CONSTRAINT PK_DataModel_CustomerOrders PRIMARY KEY
+        CONSTRAINT DF_DataModel_CustomerOrders_CustomerOrderKey
+            DEFAULT (NEXT VALUE FOR DataModel.CustomerOrderKey),
+    CustomerOrderID int NOT NULL,
+    CustomerKey int NOT NULL,
+    OrderDate date NOT NULL,
+    CustomerPurchaseOrderNumber varchar(20) NULL,
+    Comments1 varchar(35) NULL,
+    Comments2 varchar(35) NULL,
+    Comments3 varchar(35) NULL,
+    Comments4 varchar(35) NULL,
+    ExpectedDeliveryDate date NULL,
+    DeliveryInstruction1 varchar(35) NULL,
+    DeliveryInstruction2 varchar(35) NULL,
+    DeliveryInstruction3 varchar(35) NULL,
+    DeliveryRunCode varchar(8) NULL,
+    DeliveryRunPosition int NULL,
+    TransportCompanySupplierKey int NOT NULL,
+    HasBeenPicked bit NOT NULL,
+    OrderingStaffMemberKey int NOT NULL,
+    PickingStaffMemberKey int NULL
+);
+GO
