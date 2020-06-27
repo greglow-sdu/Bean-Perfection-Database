e00f77c463dd11c66e0f7a41d54b3a9bb03341b9
Greg Low <greg@sqldownunder.com>
2020-06-27 09:52:37 +0000

    Added Sequence_DataModel_BuyingGroups.sql

---
+++ Sequence_DataModel_BuyingGroups.sql
@@ -0,0 +1,11 @@
+IF NOT EXISTS (SELECT 1 FROM sys.sequences AS seq
+                        INNER JOIN sys.schemas AS sch
+                        ON sch.schema_id = seq.schema_id 
+                        WHERE sch.[name] = N'DataModel'
+                        AND seq.[name] = N'BuyingGroupKey')
+BEGIN
+    DECLARE @SQL nvarchar(max) = N'CREATE SEQUENCE DataModel.BuyingGroupKey AS int START WITH 1;';
+    EXEC (@SQL);
+END;
+GO
+
