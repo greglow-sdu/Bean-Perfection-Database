15c88413a56bb431164a49ec06518bd0fdf060fd
Greg Low <greg@sqldownunder.com>
2020-06-27 10:54:14 +0000

    Added Table_DataModel_CustomerOrderLines.sql

---
+++ Table_DataModel_CustomerOrderLines.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS DataModel.CustomerOrderLines;
+GO
+
+CREATE TABLE DataModel.CustomerOrderLines
+(
+    CustomerOrderLineKey int NOT NULL
+        CONSTRAINT PK_DataModel_CustomerOrderLines PRIMARY KEY
+        CONSTRAINT DF_DataModel_CustomerOrderLines_CustomerOrderLineKey
+            DEFAULT (NEXT VALUE FOR DataModel.CustomerOrderLineKey),
+    CustomerOrderKey int NOT NULL,
+    OrderLineNumber int NOT NULL,
+    StockItemKey int NOT NULL,
+    OrderItemDescription varchar(50) NOT NULL,
+    OrderedQuantity int NOT NULL,
+    PickedQuantity int NOT NULL,
+    SellingPackageKey int NOT NULL,
+    UnitPrice decimal(18, 2) NOT NULL,
+    DiscountPercentage decimal(18, 3) NOT NULL,
+    DiscountAmount decimal(18, 2) NOT NULL,
+    TaxRate decimal(18, 3) NOT NULL
+);
+GO
